1616970f18cedfa8ddcae2080545dc692f554681
hexvalid <hexvalid@gmail.com>
2020-02-07 19:20:34 +0300

    Added more tests

--- main_test.go
+++ main_test.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"github.com/veandco/go-sdl2/sdl"
+	"math/rand"
 	"testing"
+	"time"
 )
 
+// Test Game Table Printing
+func TestGamePrint(t *testing.T) {
+	g := NewGame(3, 3)
+	g.SetCell(0, 0, true)
+	g.SetCell(1, 1, true)
+	g.SetCell(2, 2, true)
+	t.Run("Print Game Table to Console", func(t *testing.T) {
+		g.DebugPrint()
+	})
+}
+
+// Test Tables
 func TestGameTableEquality(t *testing.T) {
 	g := NewGame(3, 3)
 	g.SetCell(2, 1, true)
@@ -35,7 +49,8 @@ func TestGameRules(t *testing.T) {
 		0 0 0
 	*/
 
-	g1.Run() //not yet implemented
+	g1.CreatePlan()
+	g1.RunPlan()
 	if !g1.Equals(g1Target) {
 		t.Error("RULE 1 failed")
 	}
@@ -65,7 +80,8 @@ func TestGameRules(t *testing.T) {
 		0 1 1
 	*/
 
-	g2.Run() //not yet implemented
+	g2.CreatePlan()
+	g2.RunPlan()
 	if !g2.Equals(g2Target) {
 		t.Error("RULE 2 failed")
 	}
@@ -98,7 +114,8 @@ func TestGameRules(t *testing.T) {
 		1 0 1
 	*/
 
-	g3.Run() //not yet implemented
+	g3.CreatePlan()
+	g3.RunPlan()
 	if !g3.Equals(g3Target) {
 		t.Error("RULE 3 failed")
 	}
@@ -123,13 +140,37 @@ func TestGameRules(t *testing.T) {
 		0 1 0
 	*/
 
-	g4.Run() //not yet implemented
+	g4.CreatePlan()
+	g4.RunPlan()
 	if !g4.Equals(g4Target) {
 		t.Error("RULE 4 failed")
 	}
 
 }
 
+func TestGameRandomize(t *testing.T) {
+	var gs []game
+	var equalLen int
+	var testCount = 256
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < testCount; i++ {
+		g := NewGame(64, 64)
+		g.Randomize()
+		// Check equality of before generated game tables
+		for a := range gs {
+			if gs[a].Equals(g) {
+				equalLen++
+			}
+		}
+		gs = append(gs, *g)
+	}
+	if equalLen > testCount/100 {
+		t.Errorf("Too match equal game tables. Seeding may be not working well. "+
+			"Generated tables: %d Equal tables: %d", testCount, equalLen)
+	}
+}
+
+// Test SDL Library
 func TestSDL(t *testing.T) {
 	// These are the flags which may be passed to SDL_Init()
 	// Reference: https://wiki.libsdl.org/SDL_Init
