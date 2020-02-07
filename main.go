523559f4cfa0578b5f2b909ab23ea27e63e38ef4
hexvalid <hexvalid@gmail.com>
2020-02-07 16:47:20 +0300

    Game struct, getter and setter

--- main.go
+++ main.go
@@ -2,6 +2,61 @@ package main
 
 import "fmt"
 
+type game struct {
+	xSize int
+	ySize int
+	cells []bool
+}
+
+func NewGame(xSize, ySize int) *game {
+	return &game{
+		xSize: xSize,
+		ySize: ySize,
+		cells: make([]bool, xSize*ySize),
+	}
+}
+
+func (g *game) SetCell(xPos, yPos int, val bool) {
+	g.cells[yPos*(g.xSize)+xPos] = val
+}
+
+func (g *game) GetCell(xPos, yPos int) bool {
+	return g.cells[yPos*(g.xSize)+xPos]
+}
+
+// Compare game to another game for unit tests.
+func (g *game) Equals(tg *game) bool {
+	// Check game tables and cell sizes are equal
+	if g.xSize != tg.xSize || g.ySize != tg.ySize || len(g.cells) != len(tg.cells) {
+		return false
+	}
+
+	// Check all cell states
+	for k := range g.cells {
+		if g.cells[k] != tg.cells[k] {
+			return false
+		}
+	}
+	return true
+}
+
+// Prints game table to terminal. Useful for debugging.
+func (g *game) DebugPrint() {
+	for x := 0; x < g.xSize; x++ {
+		for y := 0; y < g.ySize; y++ {
+			if g.cells[y*(g.xSize)+x] {
+				fmt.Print("1")
+			} else {
+				fmt.Print("0")
+			}
+			fmt.Print(" ")
+		}
+		fmt.Print("\n")
+	}
+}
+
 func main() {
-	fmt.Print("Hello World")
+	g := NewGame(3, 3)
+	g.SetCell(0, 0, true)
+	g.DebugPrint()
 }
--- main_test.go
+++ main_test.go
@@ -3,8 +3,17 @@ package main
 import (
 	"github.com/veandco/go-sdl2/sdl"
 	"testing"
-	"time"
 )
+
+func TestGameEquality(t *testing.T) {
+	g1 := NewGame(3, 3)
+	g1.SetCell(2, 1, true)
+	g2 := NewGame(3, 3)
+	g2.SetCell(2, 1, true)
+	if !g1.Equals(g2) {
+		t.Error("Game tables not equal")
+	}
+}
 
 func TestSDL(t *testing.T) {
 	// These are the flags which may be passed to SDL_Init()
@@ -12,6 +21,9 @@ func TestSDL(t *testing.T) {
 	if err := sdl.Init(sdl.INIT_VIDEO); err != nil {
 		t.Errorf("SDL is not correctly initialized. Error: %s", err)
 	}
+
+	defer sdl.Quit() // Don't forget quit. Prevents memory leak.
+
 	window, err := sdl.CreateWindow("Test", 0, 0, 128, 128, sdl.WINDOW_SHOWN)
 	if err != nil {
 		t.Errorf("SDL window is not correctly created. Error: %s", err)
@@ -20,7 +32,7 @@ func TestSDL(t *testing.T) {
 	if err != nil {
 		t.Errorf("Can't get surface of SDL window. Error: %s", err)
 	}
-	time.Sleep(500 * time.Millisecond)
+
 	bg := sdl.Rect{X: 0, Y: 0, W: 128, H: 128}
 	err = surface.FillRect(&bg, 0xAAAAAAAA)
 	if err != nil {
@@ -31,8 +43,6 @@ func TestSDL(t *testing.T) {
 		t.Errorf("Can't update surface of SDL window. Error: %s", err)
 	}
 
-	time.Sleep(1 * time.Second) // Humanize
-
 	rect := sdl.Rect{X: int32(32), Y: int32(32), W: 64, H: 64}
 	err = surface.FillRect(&rect, 0x00991111)
 	if err != nil {
@@ -42,8 +52,6 @@ func TestSDL(t *testing.T) {
 	if err != nil {
 		t.Errorf("Can't update surface of SDL window. Error: %s", err)
 	}
-
-	time.Sleep(1 * time.Second) //Humanize
 
 	err = window.Destroy()
 	if err != nil {
